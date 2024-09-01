bfc9ca6ba2331a67f397f80b3b30ef14ea967c9b
anthony david tom <anthodave01@gmail.com>
2024-09-01 18:55:36 +0100

    second commit

---
+++ src/components/hero/page.jsx
@@ -0,0 +1,11 @@
+import "./hero.css"
+
+const Hero = () => {
+      return (
+            <div className="Hero">
+                  <h1 className="HeroHeading"> Limitless Succession <br/> All Round</h1>
+            </div>
+      )
+}
+
+export default Hero;
